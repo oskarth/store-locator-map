2aa38570b5b14fd6b47cb9eaa0f68d4b1eded534
Oskar Thoren <o.thoren@gmail.com>
2013-06-13 15:56:55 +0200

    Basic google map.

---
+++ map.js
@@ -0,0 +1,12 @@
+var map;
+function initialize() {
+    var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(59.325, 18.05),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+                              mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
